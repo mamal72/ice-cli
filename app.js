caad5fa9568c36a5c93e6ec80039d239ac9e1d28
Mohamad Jahani <m4m4lj@gmail.com>
2017-06-09 01:27:53 +0430

    fix: convert year and month value to int

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ const prettyPrint = data => JSON.stringify(data, null, 4);
 program
   .version(version)
   .arguments('<year> [month]')
-  .action(async (year, month) => {
+  .action(async (y, m) => {
+    // convert year and month to int
+    const year = parseInt(y, 10);
+    const month = parseInt(m, 10);
+    // final events output
     let events = [];
+    // year or month events
     if (!month) {
+      // year events
       events = await ice({ year });
     } else {
+      // month events
       events = await ice({ year, month });
     }
-    console.log(prettyPrint(events));
+    // pretty print events
+    return console.log(prettyPrint(events));
   })
   .parse(process.argv);
